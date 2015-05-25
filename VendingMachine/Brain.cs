abae806413af2ae47b3970bc94b00f24b2af0b02
MalcolmAnderson <malcolm.b.anderson@pragmaticagility.com>
2015-05-25 13:30:52 -0700

    First Add
    First add to github

---
+++ VendingMachine/Brain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VendingMachine
+{
+    public class Brain
+    {
+        //As a vendor
+        //I want a vending machine that accepts coins
+        //So that I can collect money from the customers
+    }
+}
---
+++ VendingMachineTests/VendingMachineTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace VendingMachine
+{
+    [TestClass]
+    public class BrainTest
+    {
+        [TestMethod]
+        public void ShouldInstantiate()
+        {
+            Brain o = new Brain();
+
+        }
+    }
+}
